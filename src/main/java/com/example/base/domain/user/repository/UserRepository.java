4382823f0c77253a6528fb4c8675439e6e517a9b
can019 <jys01012@gmail.com>
2024-05-23 22:28:49 +0900

    feat: User repository에 user create, find one 기능 작성

---
+++ src/main/java/com/example/base/domain/user/repository/UserRepository.java
@@ -0,0 +1,28 @@
+package com.example.base.domain.user.repository;
+
+
+import com.example.base.domain.user.domain.User;
+import jakarta.persistence.EntityManager;
+import jakarta.persistence.PersistenceContext;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public class UserRepository  {
+
+    @PersistenceContext
+    EntityManager em;
+
+    public void save(User user){
+        Logger logger = LoggerFactory.getLogger(UserRepository.class);
+        em.persist(user);
+    }
+
+    public User findOne(byte[] id){
+        return em.find(User.class, id);
+    }
+
+}
