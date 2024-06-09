8e4f1f792c74b4bdee10e470d24ea1623e12371e
can019 <jys01012@gmail.com>
2024-06-09 22:24:51 +0900

    feat: `GenerateThreadContextIdFilter`에서 `Thread context`에 id 부여   - 더 이상 `LoggingInterceptor`에서 부여하지 않음   - Ingergation, unit test 작성

---
+++ src/test/integration/java/com/example/base/global/filter/GenerateThreadContextIdFilterTest.java
@@ -0,0 +1,33 @@
+package com.example.base.global.filter;
+
+import com.example.base.global.filter.mock.GenerateThreadContextIdTestMockController;
+import org.apache.logging.log4j.ThreadContext;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.context.WebApplicationContext;
+
+import java.util.UUID;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
+
+@WebMvcTest(controllers = {GenerateThreadContextIdTestMockController.class})
+public class GenerateThreadContextIdFilterTest {
+    private MockMvc mockMvc; // 특정 filter만을 target으로 하기 위해 Autowired를 사용하지 않음.
+    @Autowired
+    protected WebApplicationContext context;
+
+    @Test
+    void controller에서_thread_id는_같아야한다() throws Exception {
+        mockMvc = MockMvcBuilders.webAppContextSetup(context)
+                .addFilters(new GenerateThreadContextIdFilter()).build();
+        String randomId = UUID.randomUUID().toString().substring(0,8);
+
+        ThreadContext.put("id", randomId);
+        mockMvc.perform(get("/foo"))
+                .andExpect(jsonPath("$").value(randomId));
+    }
+}
---
+++ src/test/integration/java/com/example/base/global/filter/mock/GenerateThreadContextIdTestMockController.java
@@ -0,0 +1,20 @@
+package com.example.base.global.filter.mock;
+
+
+import org.apache.logging.log4j.ThreadContext;
+import org.springframework.context.annotation.Profile;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Profile({"!production" , "prod"})
+// 또는 @Conditional 사용
+@RestController
+public class GenerateThreadContextIdTestMockController {
+
+    @GetMapping("/foo")
+    public ResponseEntity foo() {
+        return ResponseEntity.status(HttpStatus.OK).body(ThreadContext.get("id").toString());
+    }
+}
