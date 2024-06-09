be844b966283bfb11c9091333e9c394288e9f30d
can019 <jys01012@gmail.com>
2024-06-10 00:19:35 +0900

    chore: 필요없는 import 정리

--- src/test/integration/java/com/example/base/global/filter/GenerateThreadContextIdFilterTest.java
+++ src/test/integration/java/com/example/base/global/filter/GenerateThreadContextIdFilterTest.java
@@ -6,7 +6,6 @@ import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 import org.springframework.web.context.WebApplicationContext;
