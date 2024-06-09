fabad84b8bd462d9fe110e5c6c0576bedfae962e
can019 <jys01012@gmail.com>
2024-06-09 23:58:32 +0900

    feat: Generate thread id test profile 변경

--- src/test/integration/java/com/example/base/global/filter/mock/GenerateThreadContextIdTestMockController.java
+++ src/test/integration/java/com/example/base/global/filter/mock/GenerateThreadContextIdTestMockController.java
@@ -8,7 +8,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@Profile({"!production" , "prod"})
+@Profile("GENERATE_THREAD_CONTEXT_ID_TEST")
 // 또는 @Conditional 사용
 @RestController
 public class GenerateThreadContextIdTestMockController {
