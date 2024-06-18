3d962bfe3f067871564dfb1139f693c8cddb8949
can019 <jys01012@gmail.com>
2024-06-18 21:58:33 +0900

    chore: /user (post) test 통과

--- src/test/integration/java/com/example/base/domain/user/controller/UserControllerTest.java
+++ src/test/integration/java/com/example/base/domain/user/controller/UserControllerTest.java
@@ -1,8 +1,14 @@
 package com.example.base.domain.user.controller;
 
+import com.example.base.domain.user.domain.User;
+import com.example.base.global.util.convertor.TypeConvertor;
+import jakarta.persistence.EntityManager;
+import net.minidev.json.JSONObject;
+import net.minidev.json.parser.JSONParser;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import com.example.base.test.util.WithContextControllerTestBase;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MvcResult;
 
@@ -12,11 +18,21 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.assertj.core.api.Assertions.assertThat;
 
 public class UserControllerTest extends WithContextControllerTestBase {
+    @Autowired
+    EntityManager em;
 
     @Test
-    @DisplayName("유저 생성 (api로 생성 후 데이터 베이스 raw query로 저장 여부 확인), [Post] /user")
+    @DisplayName("유저 생성 (api로 생성 후 entityManager로 저장 여부 확인), [Post] /user")
     void createUser() throws Exception{
-        mockMvc.perform(post("/user")).andExpect(status().isCreated())
-                .andExpect(jsonPath("$.id").isNotEmpty());
+        MvcResult mvcResult = mockMvc.perform(post("/user")).andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNotEmpty()).andReturn();
+
+        JSONParser jsonParser = new JSONParser();
+        JSONObject json = (JSONObject)jsonParser.parse(mvcResult.getResponse().getContentAsString());
+
+        byte[] id = TypeConvertor.hexStringToByte((String) json.get("id"));
+
+        User user = em.find(User.class, id);
+        assertThat(user.getId()).isEqualTo(id);
     }
 }
