d164477b3f20c8685988c375112c02756b9bd3b5
can019 <jys01012@gmail.com>
2024-06-18 21:05:10 +0900

    feat: hexStringToByte() 작성   - HexString을 byte로 변환   - input이 hexString이 아니라면 NumberFormatException 발생     - thorwn by java   - input.length()가 16~32가 아니라면 StringIndexOutOfBoundsException 발생     - thrown by developer

--- src/main/java/com/example/base/global/util/convertor/TypeConvertor.java
+++ src/main/java/com/example/base/global/util/convertor/TypeConvertor.java
@@ -1,5 +1,7 @@
 package com.example.base.global.util.convertor;
 
+import java.nio.ByteBuffer;
+
 public class TypeConvertor {
     private static final char[] HEX_ARRAY = "0123456789ABCDEF".toCharArray();
 
@@ -11,5 +13,20 @@ public class TypeConvertor {
             hexChars[i * 2 + 1] = HEX_ARRAY[v & 0x0F];
         }
         return new String(hexChars).toLowerCase();
+    }
+
+    /**
+     *
+     * @param str 숫자로만 이루어져야 하며 16자가 넘어야 합니다.
+     * @return
+     */
+    public static byte[] hexStringToByte(String str){
+        if(str.length() < 16) throw new StringIndexOutOfBoundsException("Input string length should over 16");
+        if(str.length() > 32) throw new StringIndexOutOfBoundsException("Input string length should under 32");
+
+        ByteBuffer bb = ByteBuffer.wrap(new byte[16]);
+        bb.putLong(Long.parseUnsignedLong(str.substring(0, 16), 16));
+        bb.putLong(Long.parseUnsignedLong(str.substring(16),16));
+        return bb.array();
     }
 }
