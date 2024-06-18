7dc7d29710e06c4c4d13c98e14d1a9d755474b46
can019 <jys01012@gmail.com>
2024-06-18 21:07:33 +0900

    refacotr: hexStringToByte() thorws NumberFormatException 명시

--- src/main/java/com/example/base/global/util/convertor/TypeConvertor.java
+++ src/main/java/com/example/base/global/util/convertor/TypeConvertor.java
@@ -20,7 +20,7 @@ public class TypeConvertor {
      * @param str 숫자로만 이루어져야 하며 16자가 넘어야 합니다.
      * @return
      */
-    public static byte[] hexStringToByte(String str){
+    public static byte[] hexStringToByte(String str) throws NumberFormatException{
         if(str.length() < 16) throw new StringIndexOutOfBoundsException("Input string length should over 16");
         if(str.length() > 32) throw new StringIndexOutOfBoundsException("Input string length should under 32");
 
