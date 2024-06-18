3a20fe9f99a72113f44204aaa2296b7f8b225651
can019 <jys01012@gmail.com>
2024-06-19 01:04:46 +0900

    chore: /user (get, post) test 추가

--- src/test/integration/java/com/example/base/domain/user/controller/UserControllerTest.java
+++ src/test/integration/java/com/example/base/domain/user/controller/UserControllerTest.java
@@ -1,7 +1,6 @@
 package com.example.base.domain.user.controller;
 
 import com.example.base.domain.user.domain.User;
-import com.example.base.global.util.convertor.TypeConvertor;
 import jakarta.persistence.EntityManager;
 import net.minidev.json.JSONObject;
 import net.minidev.json.parser.JSONParser;
@@ -9,10 +8,11 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import com.example.base.test.util.WithContextControllerTestBase;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.test.web.servlet.MvcResult;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static com.example.base.global.util.convertor.TypeConvertor.byteArrayToHexString;
+import static com.example.base.global.util.convertor.TypeConvertor.hexStringToByte;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 import static org.assertj.core.api.Assertions.assertThat;
@@ -30,9 +30,40 @@ public class UserControllerTest extends WithContextControllerTestBase {
         JSONParser jsonParser = new JSONParser();
         JSONObject json = (JSONObject)jsonParser.parse(mvcResult.getResponse().getContentAsString());
 
-        byte[] id = TypeConvertor.hexStringToByte((String) json.get("id"));
+        byte[] id = hexStringToByte((String) json.get("id"));
 
         User user = em.find(User.class, id);
         assertThat(user.getId()).isEqualTo(id);
+    }
+
+    @Test
+    @DisplayName("단일 유저 조회 (데이터 베이스 entityManager로 생성 후 api로 조회) | [Get] /user")
+    void getUser() throws Exception{
+        User user = new User();
+        em.persist(user);
+        em.flush();
+
+        mockMvc.perform(get("/user")
+                        .param("id", byteArrayToHexString(user.getId())))
+                        .andExpect(status().isOk())
+                        .andExpect(jsonPath("$.id").value(user.getIdAsString()));
+    }
+
+    @Test
+    @DisplayName("단일 유저 조회 (api로 user 생성 후 api 조회)| [Get] /user -> [Post] /user")
+    void createUserAndGetUser() throws Exception{
+        MvcResult mvcResult = mockMvc.perform(post("/user")).andExpect(status().isCreated())
+                .andExpect(jsonPath("$.id").isNotEmpty()).andReturn();
+
+        JSONParser jsonParser = new JSONParser();
+        JSONObject json = (JSONObject)jsonParser.parse(mvcResult.getResponse().getContentAsString());
+
+        byte[] id = hexStringToByte((String) json.get("id"));
+
+        mockMvc.perform(get("/user")
+                        .param("id", byteArrayToHexString(id)))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.id").value(json.get("id")));
+
     }
 }
