c2c0bbaa7ffa15ab93ef792e4ac37cdd79e041a9
can019 <jys01012@gmail.com>
2024-06-18 15:39:52 +0900

    chore: 패키지 구조 변경

--- src/test/integration/java/com/example/base/test/util/WithContextControllerTestBase.java
+++ src/test/integration/java/com/example/base/test/util/WithContextControllerTestBase.java
@@ -1,6 +1,7 @@
-package test.util;
+package com.example.base.test.util;
 
 
+import com.example.base.BaseApplication;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Disabled;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,14 +10,14 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.transaction.annotation.Transactional;
 
-@SpringBootTest
+@SpringBootTest(classes = BaseApplication.class)
 @Disabled
 // Servlet container mocking
 // @SpringTest와 WebMvc를 사용하는 경우 사용. @WebMvcTest과 함께 쓰면 각자 서로의 MockMvc를 모킹하려 하기 때문에 충돌
 // @WebMvc는 타겟 클래스만 인스턴스화 하지만 @AuthConfigureMockMvc는 관련 service, repository 모두 인스턴스화.
 @AutoConfigureMockMvc
 @Transactional
-public class WithContextControllerTestBase {
+public abstract class WithContextControllerTestBase {
     @Autowired
     protected MockMvc mockMvc;
 
