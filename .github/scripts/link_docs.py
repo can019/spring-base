e270bfb019558a70833c44df2120714491e2f3ab
can019 <jys01012@gmail.com>
2024-06-20 00:39:33 +0900

    feat: Release가 생성되면 release docs 생성

--- .github/scripts/link_docs.py
+++ .github/scripts/link_docs.py
@@ -7,42 +7,44 @@ from file_util import *
 
 ## global var
 root_dir = "docs"
-commit_id = ""
-commit_dir_path = ""
+identifier = ""
+identifier_dir_path = ""
 config_path = ".github/config/gh-pages.yml"
 global_config = {}
 
+mode = ""
 latest_pattern = r"### Latest\n(.+?)(?=\n- - -)"
 other_pattern = r"### Other\n(.+?)(?=\n- - -)"
 
 
 
-def init_develop_markdown():
-    develop_config = get_config_by_name("develop", global_config.get("main"))
-    description = develop_config.get("description")
-    content = f"## develop\n### description\n{description}\n\n"
+def init_second_root_markdown():
+    name_config = get_config_by_name(mode, global_config.get("main"))
+    print(name_config)
+    description = name_config.get("description")
+    content = f"## {mode}\n### description\n{description}\n\n"
     content += "### Latest\n<!-- Latest -->\n\n"
     content += "### Other\n<!-- Other --> \n\n"
 
-    create_markdown_file('/'.join([root_dir,"develop","index.md"]), content)
+    create_markdown_file('/'.join([root_dir,mode ,"index.md"]), content)
 
-def update_develop_markdown(text):
-    develop_markdown_path = '/'.join([root_dir,"develop" ,"index.md"])
-    if not os.path.exists(develop_markdown_path):
-        init_develop_markdown()
+def update_second_root_markdown(text):
+    second_root_markdown_path = '/'.join([root_dir, mode,"index.md"])
+    if not os.path.exists(second_root_markdown_path):
+        init_second_root_markdown()
 
-    latest_line = find_line_number(develop_markdown_path,"## Latest")
-    latest_end_line =  find_line_number(develop_markdown_path,"<!-- Latest -->", start_line=latest_line)
+    latest_line = find_line_number(second_root_markdown_path,"## Latest")
+    latest_end_line =  find_line_number(second_root_markdown_path,"<!-- Latest -->", start_line=latest_line)
 
-    other_line = find_line_number(develop_markdown_path,"### Other")
+    other_line = find_line_number(second_root_markdown_path,"### Other")
 
     if latest_end_line - latest_line == 1 :
         # 현재 Latest가 비어있는 경우
-        add_line_to_file(develop_markdown_path, latest_line +1 , text)
+        add_line_to_file(second_root_markdown_path, latest_line +1 , text)
     else:
         # Latest가 존재하는 경우
-        orign_text = replace_line_in_file(develop_markdown_path,latest_line +1, text)
-        add_line_to_file(develop_markdown_path, other_line +1 , orign_text)
+        orign_text = replace_line_in_file(second_root_markdown_path,latest_line +1, text)
+        add_line_to_file(second_root_markdown_path, other_line +1 , orign_text)
 
 def get_exist_latest():
     exist_content = read_markdown('/'.join([root_dir, "index.md"]))
@@ -65,7 +67,6 @@ def get_exist_other():
 
     return other_match.group(1).strip()
 
-
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
         prog="Generate docs resource",
@@ -74,18 +75,20 @@ if __name__ == "__main__":
     )
 
     ## init set up
-    parser.add_argument("-i", "--id", type=str, help="Commit id", required=True)
+    parser.add_argument("-i", "--id", type=str, help="Commit id or release version", required=True)
+    parser.add_argument("-t", "--type", type=str, choices=["release", "develop"], help="Commit id (release or develop)", required=True)
     args = parser.parse_args()
 
-    commit_id = args.id
-    commit_dir_path = '/'.join([root_dir, "develop", args.id])
+    identifier = args.id # commit id or release tag
+    mode = args.type
+    identifier_dir_path = '/'.join([root_dir, mode, identifier]) # docs/${mode}/${identifier}
 
     global_config = read_yaml(config_path)
 
+    create_folder(identifier_dir_path)
+    create_id_markdown('/'.join([identifier_dir_path, "index.md"]), global_config.get('list'))
 
-    create_folder(commit_dir_path)
-    create_commit_id_markdown('/'.join([commit_dir_path, "index.md"]), global_config.get('list'))
+    link = create_markdown_link_text(identifier, f"{identifier_dir_path}/index.md") # [$identifier]($identifier/index.md)
 
-
-    update_develop_markdown(create_markdown_list(create_markdown_link_text(commit_id, f"{commit_id}/index.md")))
+    update_second_root_markdown(create_markdown_list(link))
 
