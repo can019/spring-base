c78df948bf8a63dcaa659be119ffe0337d3d18cd
can019 <jys01012@gmail.com>
2024-06-26 18:35:14 +0900

    feat: Identifier, SequenceId 추가   - User id 조작 시 side effect 제거를 위한 class     - User id는 hex byte[] 형식     - toSting(), getBytes()등의 연산을 하면 안됨     - 이를 막기 위해 Interface Identifier, Class SequenceId 추가     - user.id를 직접적으로 접근 (getter, setter)가 불가, getIdentifier()를 통해 접근     - Identifier는 getIdAsBytes, getIdAsString을 통해 접근하도록 함.

---
+++ src/main/java/com/example/base/global/identifier/SequenceId.java
@@ -0,0 +1,51 @@
+package com.example.base.global.identifier;
+
+import com.example.base.global.util.convertor.TypeConvertor;
+import com.example.base.global.util.generator.UUID;
+
+import java.util.Objects;
+
+public class SequenceId implements Identifier{
+    private final byte[] id;
+    private String strId;
+
+    public SequenceId(byte[] id){
+        this.id = id;
+    }
+
+    public SequenceId(){
+        this(UUID.generateSequentialUUIDV1WithoutHyphenAsByte());
+    }
+
+    private byte[] getId(){
+        return this.id;
+    }
+
+    @Override
+    public String getIdAsString() {
+        if(strId == null){
+            strId = TypeConvertor.byteArrayToHexString(this.getId());
+        }
+        return strId;
+    }
+
+    @Override
+    public byte[] getIdAsByte() {
+        return this.getId();
+    }
+
+    @Override
+    public boolean equals(Object target) {
+        if (this == target)
+            return true;
+        if (!(target instanceof SequenceId))
+            return false;
+        SequenceId targetSequenceId = (SequenceId)target;
+        return this.id == targetSequenceId.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
+    }
+}
