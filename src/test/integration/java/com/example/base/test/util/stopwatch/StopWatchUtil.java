d50c11b9b25ea8835466b33d92afb0f0633bae1d
can019 <jys01012@gmail.com>
2024-06-28 13:11:48 +0900

    feat: StopWatch 결과를 csv로 export하는 StopWatchUil class 작성

---
+++ src/test/integration/java/com/example/base/test/util/stopwatch/StopWatchUtil.java
@@ -0,0 +1,48 @@
+package com.example.base.test.util.stopwatch;
+
+import org.springframework.util.StopWatch;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.List;
+
+public class StopWatchUtil {
+
+    public static void exportCsv(StopWatch stopWatch, String filePath) {
+        Path path = Paths.get(filePath);
+        Path directoryPath = path.getParent();
+
+        createDirectory(directoryPath);
+
+        List<StopWatch.TaskInfo> taskInfoList = List.of(stopWatch.getTaskInfo());
+
+        // CSV 파일 쓰기
+        try (PrintWriter writer = new PrintWriter(new FileWriter(filePath))) {
+            // CSV 파일 헤더 작성
+            writer.println("Task Name,times,Total Time (nano second)");
+
+            for (StopWatch.TaskInfo taskInfo : taskInfoList) {
+                String taskName = taskInfo.getTaskName();
+                long totalTimeMillis = taskInfo.getTimeNanos();
+
+                writer.println(taskName +"," + totalTimeMillis);
+            }
+            System.out.println("CSV report created");
+        } catch (IOException e) {
+            System.err.println("Error occurred while creating CSV report " + e.getMessage());
+        }
+    }
+
+    private static void createDirectory(Path path) {
+        try {
+            Files.createDirectories(path);
+            System.out.println("Directory created successfully: " + path);
+        } catch (IOException e) {
+            System.err.println("Error creating directory: " + e.getMessage());
+        }
+    }
+}
