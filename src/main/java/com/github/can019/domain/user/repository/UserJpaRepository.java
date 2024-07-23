2ea35920b5e8ead551897f83a1c17fa7f3a00d0f
can019 <jys01012@gmail.com>
2024-07-23 11:30:42 +0900

    chore: 예제로 작성했던 user 관련 기능 삭제

--- src/main/java/com/github/can019/domain/user/repository/UserJpaRepository.java
+++
@@ -1,7 +0,0 @@
-package com.github.can019.domain.user.repository;
-
-import com.github.can019.domain.user.domain.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserJpaRepository extends JpaRepository<User, byte[]> {
-}
