b68503524f9444fba01cea9b00ac4d9414ddb520
can019 <jys01012@gmail.com>
2024-07-30 21:39:06 +0900

    fix: Config url이 openapi로 설정된 버그 픽스

--- docs/swagger-ui/dist/swagger-initializer.js
+++ docs/swagger-ui/dist/swagger-initializer.js
@@ -3,7 +3,7 @@ window.onload = function() {
 
   // the following lines will be replaced by docker/configurator, when it runs in a docker-container
   window.ui = SwaggerUIBundle({
-    configUrl:"https://can019.github.io/spring-base/release/openapi3.yaml",
+    configUrl:"https://can019.github.io/spring-base/swagger-config.yaml",
     dom_id: '#swagger-ui',
     deepLinking: true,
     presets: [
