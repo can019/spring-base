3b24482ffc9201e9b3f1049c182e877c172490e6
can019 <jys01012@gmail.com>
2024-08-01 02:14:05 +0900

    feat: Test 수행 시 시간 측정 후 csv export하는 TestExecutionListener - TotalTestTimeExecutionListener : 총 수행 시간 측정 - BasicTestTimeExecutionListener: 각 method마다 자동으로 측정 (Non-thread-safe) - ParalleTestTimeExecutionListener: 각 method마다 자동으로 측정 (Thread-safe)    - parallel 가능    - `@RepeatedTest` + parallel은 불가

---
+++ src/integration/java/com/github/can019/test/util/listener/performance/BasicTestTimeExecutionListener.java
@@ -0,0 +1,39 @@
+package com.github.can019.test.util.listener.performance;
+
+import com.github.can019.test.util.report.StopWatchReporter;
+import org.springframework.test.context.TestContext;
+import org.springframework.test.context.support.AbstractTestExecutionListener;
+import org.springframework.util.StopWatch;
+
+import static com.github.can019.test.util.report.StopWatchReporter.exportReport;
+
+/**
+ * Non thread safe
+ * 자동으로 각 test method의 수행 시간을 측정, csv로 export
+ * `@Execution(value = ExecutionMode.SAME_THREAD)`에서만 사용 가능
+ */
+public class BasicTestTimeExecutionListener extends AbstractTestExecutionListener {
+    private StopWatch stopWatch;
+
+    @Override
+    public void beforeTestClass(TestContext testContext) throws Exception {
+        super.beforeTestClass(testContext);
+        stopWatch = new StopWatch(testContext.getTestClass().getSimpleName());
+    }
+
+    @Override
+    public void beforeTestMethod(TestContext testContext) throws Exception {
+        super.beforeTestMethod(testContext);
+        stopWatch.start(testContext.getTestMethod().getName());
+    }
+
+    @Override
+    public void afterTestMethod(TestContext testContext) throws Exception {
+        if (stopWatch.isRunning()) {
+            stopWatch.stop();
+        }
+        super.afterTestMethod(testContext);
+
+        exportReport(stopWatch, "total", testContext.getTestClass(),  StopWatchReporter.ReportType.CSV);
+    }
+}
--- src/integration/java/com/github/can019/test/util/listener/performance/TotalTestTimeExecutionListener.java
+++ src/integration/java/com/github/can019/test/util/listener/performance/TotalTestTimeExecutionListener.java
@@ -1,9 +1,12 @@
 package com.github.can019.test.util.listener.performance;
 
+import com.github.can019.test.util.report.StopWatchReporter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.test.context.TestContext;
 import org.springframework.test.context.support.AbstractTestExecutionListener;
 import org.springframework.util.StopWatch;
+
+import static com.github.can019.test.util.report.StopWatchReporter.exportReport;
 
 @Slf4j
 public class TotalTestTimeExecutionListener extends AbstractTestExecutionListener {
@@ -24,5 +27,7 @@ public class TotalTestTimeExecutionListener extends AbstractTestExecutionListene
 
         log.info("The test in '{}' has been completed",testContext.getTestClass().getSimpleName());
         log.info(totalTaskStopWatch.prettyPrint());
+
+        exportReport(totalTaskStopWatch, "total", testContext.getTestClass(),  StopWatchReporter.ReportType.CSV);
     }
 }
