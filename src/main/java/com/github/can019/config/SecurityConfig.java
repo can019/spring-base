48423bb0f5722dbee745a1cf4e044b4029e63fbe
can019 <jys01012@gmail.com>
2024-08-10 23:10:33 +0900

    chore: 특정 Url에 대해 CSRF 비활성화 - login, registry, hello, actuator/**

--- src/main/java/com/github/can019/config/SecurityConfig.java
+++ src/main/java/com/github/can019/config/SecurityConfig.java
@@ -2,10 +2,12 @@ package com.github.can019.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import static org.springframework.security.config.Customizer.withDefaults;
 
@@ -15,11 +17,22 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
+                .csrf(csrf -> csrf
+                        .ignoringRequestMatchers(
+                                new AntPathRequestMatcher("/api/v0/auth/login"),
+                                new AntPathRequestMatcher("/api/v0/auth/registry"),
+                                new AntPathRequestMatcher("/actuator/**"),
+                                new AntPathRequestMatcher("/api/v0/hello")
+                        )
+                )
                 .sessionManagement(session -> session
-                        .sessionCreationPolicy(SessionCreationPolicy.STATELESS) // 세션을 사용하지 않음 (stateless)
+                        .sessionCreationPolicy(SessionCreationPolicy.IF_REQUIRED) // 세션을 사용하지 않음 (stateless)
                 )
                 .authorizeHttpRequests(authorize -> authorize
-                        .requestMatchers("/login", "/register", "/actuator/**", "/api/v0/hello").permitAll() // 로그인 및 회원가입은 허용
+                        .requestMatchers(HttpMethod.POST,"/api/v0/auth/login").permitAll()
+                        .requestMatchers(HttpMethod.POST,"/api/v0/auth/registry").permitAll()
+                        .requestMatchers("/actuator/**").permitAll()
+                        .requestMatchers(HttpMethod.GET,"/api/v0/hello").permitAll()
                         .anyRequest().authenticated() // 나머지 요청은 인증 필요
                 )
                 .httpBasic(withDefaults()); // 최신 방식의 HTTP Basic 인증 설정
