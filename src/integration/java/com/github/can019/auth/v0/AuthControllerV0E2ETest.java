d9fa8f043cdf47adce349e74ae77ab6dc7466c7a
can019 <jys01012@gmail.com>
2024-08-13 12:34:58 +0900

    chore: Auth controller v0 test의 부모 패키지에 v0 추가

--- src/integration/java/com/github/can019/auth/v0/AuthControllerV0E2ETest.java
+++ src/integration/java/com/github/can019/auth/v0/AuthControllerV0E2ETest.java
@@ -1,10 +1,9 @@
-package com.github.can019.auth;
+package com.github.can019.auth.v0;
 
 
 import com.github.can019.test.util.url.BaseUrl;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
