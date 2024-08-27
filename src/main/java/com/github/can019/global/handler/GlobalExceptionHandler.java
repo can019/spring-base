1c2343fd8eb6857c719c1a27e688b40a163dc857
Yousung Jung <jys01012@gmail.com>
2024-08-28 01:46:43 +0900

    GlobalExceptionHandler 조정 및 일부 exception 제거 (#68)
    * chore: GlobalExceptionHandler에서 handlerInternal override 삭제
    - 부여하지 않아도 된다고 판단
    
    * chore: ApplicationException, UnknownException 제거
    - 두 exception의 목표는 RFC 7807 response를 위함이었음.
      - ProblemDetail을 내부 필드로 가지는 것이 핵심이었음.
    - `ExceptionHandler`에서 ProblemDetail을 구성 할 수 있음.
    
    * test: Application, UnknownException 및 handleInternal 관련 테스트 삭제
    
    * docs: RFC7807 response 관련 doc 수정
    - 삭제된 기능에 대한 설명 제거

--- src/main/java/com/github/can019/global/handler/GlobalExceptionHandler.java
+++ src/main/java/com/github/can019/global/handler/GlobalExceptionHandler.java
@@ -1,18 +1,9 @@
 package com.github.can019.global.handler;
 
-import com.github.can019.global.exception.ApplicationException;
-import com.github.can019.global.exception.UnknownException;
-import jakarta.servlet.ServletException;
-import jakarta.servlet.http.HttpServletResponse;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.logging.log4j.ThreadContext;
-import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.*;
-import org.springframework.lang.Nullable;
-import org.springframework.web.ErrorResponse;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.ServletWebRequest;
 import org.springframework.web.context.request.WebRequest;
 import org.springframework.web.servlet.mvc.method.annotation.ResponseEntityExceptionHandler;
 
@@ -30,69 +21,5 @@ public class GlobalExceptionHandler extends ResponseEntityExceptionHandler {
     public ResponseEntity handleRuntimeException(RuntimeException ex, WebRequest webRequest) {
         HttpHeaders headers = new HttpHeaders();
         return handleExceptionInternal(ex, null, headers, HttpStatus.INTERNAL_SERVER_ERROR, webRequest);
-    }
-
-    /**
-     * 응답 body 구성에 필요한 데이터 전처리를 진행.
-     *
-     * <p>응답 body가 null이라면 ErrorResponse 객체를 가져와 body를 구성</p>
-     *
-     * @param ex cause
-     * @param body body controller에서 작성한 body
-     * @param headers
-     * @param statusCode controller에서 처리한 statusCode
-     * @param request
-     * @return
-     *
-     * @see org.springframework.web.ErrorResponse
-     */
-    @Override
-    protected ResponseEntity<Object> handleExceptionInternal(
-            Exception ex, @Nullable Object body, HttpHeaders headers, HttpStatusCode statusCode, WebRequest request) {
-
-        if (request instanceof ServletWebRequest servletWebRequest) {
-            HttpServletResponse response = servletWebRequest.getResponse();
-            if (response != null && response.isCommitted()) {
-                log.warn("Response already committed. Ignoring: {}", ex);
-                return null;
-            }
-        }
-
-        if (statusCode.equals(HttpStatus.INTERNAL_SERVER_ERROR) && body == null && !(ex instanceof ErrorResponse)) {
-            // 때에 따라 github or slack에 publish?
-            // log.error("Fatal error occurred {}", ex.getCause());
-            ex = new UnknownException(ex);
-        }
-
-        if (body == null && ex instanceof ErrorResponse errorResponse) {
-            if(ex instanceof ServletException){
-                errorResponse.getBody().setProperty("code", ExceptionCode.SERVLET_WEB_REQUEST_ERROR_CODE.getExceptionCode());
-            } else if(ex instanceof ApplicationException){
-                errorResponse.getBody().setProperty("code", ExceptionCode.APPLICATION_ERROR_CODE.getExceptionCode());
-            } else{
-                log.error("[FATAL] {} {}", ex.getMessage(), ex.getStackTrace()[0]);
-                errorResponse.getBody().setProperty("code", ExceptionCode.FATAL_ERROR_CODE.getExceptionCode());
-            }
-            errorResponse.getBody().setProperty("uuid", ThreadContext.get("id"));
-            body = errorResponse.updateAndGetBody(getMessageSource(), LocaleContextHolder.getLocale());
-        }
-
-        return createResponseEntity(body, headers, statusCode, request);
-    }
-
-    private enum ExceptionCode {
-        SERVLET_WEB_REQUEST_ERROR_CODE("EX1"),
-        APPLICATION_ERROR_CODE("EX2"),
-        FATAL_ERROR_CODE("EX3");
-
-        private String exceptionCode;
-
-        ExceptionCode(String exceptionCode){
-            this.exceptionCode = exceptionCode;
-        }
-
-        public String getExceptionCode(){
-            return exceptionCode;
-        }
     }
 }
