3a88e42179fa3e6d649ca64c12a9c84851850aab
can019 <jys01012@gmail.com>
2024-08-28 18:41:19 +0900

    feat: Admin module 생성

---
+++ admin/src/main/java/com/github/can019/base/admin/AdminApplication.java
@@ -0,0 +1,10 @@
+package com.github.can019.base.admin;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdminApplication {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
